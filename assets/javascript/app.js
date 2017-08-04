ab0a782ddbdd0db4f7439b6852fddfcba76129da
Dustin Bahr <dbahr3@dustins-mbp.uconnect.utah.edu>
2017-08-03 19:01:33 -0600

    added all links and named id fields we will be using with JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase ---------------------------------------
+  var config = {
+    apiKey: "AIzaSyBYe_ju295Nrb_OiL-P28nxg8aN4HUiQYk",
+    authDomain: "nudge-f8a04.firebaseapp.com",
+    databaseURL: "https://nudge-f8a04.firebaseio.com",
+    projectId: "nudge-f8a04",
+    storageBucket: "",
+    messagingSenderId: "146504874181"
+  };
+  firebase.initializeApp(config);
+  // Firebase ---------------------------------------------------
